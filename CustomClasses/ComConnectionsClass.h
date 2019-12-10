cb180f70853543e28cc0d863770661757d635379
AleksandrKhramov <sanya.hramov@ya.ru>
2019-12-10 11:45:43 +0300

    Added new functions in unit ComConnections: 	SearchDevices() 	IsAllActiveConnectionsExists() 	IsComListUpdated() 	UpdateActiveConnections() And started functions: 	RemoveNonexistentConnections() 	AddNewConnections()

--- CustomClasses/ComConnectionsClass.h
+++ CustomClasses/ComConnectionsClass.h
@@ -20,16 +20,22 @@ class TComConnections
     };
 
 private:
-public:
 	TComponent* Owner;
 	std::vector<TComConnection *> ComConnections;
     std::vector< std::vector<byte> > Patterns;
+    TStringList *ComNameList;
+    TStringList *ComPortList;
 
     void SearchDevices();
     void AddPattern(std::vector<byte>);
 	void DataReadyTrigger(TComConnection *, std::vector<byte>);
-    void HandingDataTrigger(std::vector<byte>);
     void ConnectionErrorTrigger(int ErrorNumber);
+    bool IsAllActiveConnectionsExists();
+    bool IsComListUpdated();
+    void UpdateActiveConnections();
+    void RemoveNonexistentConnections();
+public:
+    void HandingDataTrigger(std::vector<byte>);
     void (__closure *DataReadyForSendingTrigger)(std::vector<byte> Data);
 
     TComConnections(TComponent* _Owner);
